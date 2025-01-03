364730659b4ed441d7f99781269bb4c86b7bb024
Sean McGee <seanmcgee_dev@outlook.com>
2025-01-03 06:16:04 +0000

    fix: removing default style

--- code/js/defaultOptions.js
+++ code/js/defaultOptions.js
@@ -6,7 +6,6 @@ define([], function () {
     "duration": 10,
     "basemap": "dark-gray-vector",
     "blendMode": "normal",
-    "symbol": '%7B"type"%3A"esriSFS"%2C"color"%3A%5B255%2C255%2C255%2C255%5D%2C"outline"%3A%7B"type"%3A"esriSLS"%2C"color"%3A%5B255%2C255%2C255%2C255%5D%2C"style"%3A"esriSLSSolid"%7D%2C"style"%3A"esriSFSDiagonalCross"%7D',
     "map": {
       "extent": {
         "xmin": -1254648.3964987434,
